3b583916174591228b5829b85eae920470fc41e9
sanket9918 <sanketmohapatra13@gmail.com>
2022-12-31 09:10:54 +0530

    Fixes

--- lambda-express-typescript/app/controller/user.controller.ts
+++ lambda-express-typescript/app/controller/user.controller.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import {
-  createUser,
-  deleteUser,
-  getASingleUser,
-  getUsers,
-  updateUser,
-} from "../service/user";
+    createUser,
+    deleteUser,
+    getASingleUser,
+    getUsers,
+    updateUser,
+} from "../service/user.service";
 
 export const users = Router();
 
--- lambda-express-typescript/db/connection.ts
+++ lambda-express-typescript/db/connection.ts
@@ -5,33 +5,33 @@ import { config } from "../config/config.validator";
 
 export async function getDBConnection() {
   const sequelize = new Sequelize({
-    dialect: "postgres",
-    host: config.SEQUELIZE_DB_HOST,
-    username: config.SEQUELIZE_DB_USERNAME,
-    password: config.SEQUELIZE_DB_PASSWORD,
-    database: config.SEQUELIZE_DB_DATABASE,
-    logging: false,
-    models: [User, Hobby],
-    pool: {
-      max: 2,
-      /*
-       * Set this value to 0 so connection pool eviction logic eventually cleans up all connections
-       * in the event of a Lambda function timeout.
-       */
-      min: 0,
-      /*
-       * Set this value to 0 so connections are eligible for cleanup immediately after they're
-       * returned to the pool.
-       */
-      idle: 0,
-      // Choose a small enough value that fails fast if a connection takes too long to be established.
-      acquire: 3000,
-      /*
-       * Ensures the connection pool attempts to be cleaned up automatically on the next Lambda
-       * function invocation, if the previous invocation timed out.
-       */
-      // evict: 100,
-    },
+      dialect: "postgres",
+      host: config.SEQUELIZE_DB_HOST,
+      username: config.SEQUELIZE_DB_USERNAME,
+      password: config.SEQUELIZE_DB_PASSWORD,
+      database: config.SEQUELIZE_DB_DATABASE,
+      logging: false,
+      models: [User, Hobby],
+      pool: {
+          max: 2,
+          /*
+           * Set this value to 0 so connection pool eviction logic eventually cleans up all connections
+           * in the event of a Lambda function timeout.
+           */
+          min: 0,
+          /*
+           * Set this value to 0 so connections are eligible for cleanup immediately after they're
+           * returned to the pool.
+           */
+          idle: 0,
+          // Choose a small enough value that fails fast if a connection takes too long to be established.
+          acquire: 3000,
+          /*
+           * Ensures the connection pool attempts to be cleaned up automatically on the next Lambda
+           * function invocation, if the previous invocation timed out.
+           */
+          evict: 100,
+      },
   });
 
   await sequelize.sync();
